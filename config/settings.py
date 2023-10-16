44b843756cf3eeaa878fbae65ac482cf8be735c4
korchaginks <kkonstantinserg@gmail.com>
2023-10-16 22:03:08 +0300

    Добавил настройки для работы с приложением users

--- config/settings.py
+++ config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
 
     'main',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -147,6 +148,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
